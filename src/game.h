e34551cfbd22ebadbfda110fd8ecf327328cbc6b
carlwiede <carlwie@student.chalmers.se>
2022-03-23 21:53:24 +0100

    created game class header and skeleton of game.cpp file

--- src/game.h
+++ src/game.h
@@ -4,5 +4,36 @@
 #include "../include/glad/glad.h"
 #include "../include/GLFW/glfw3.h"
 
+// Current game state
+enum GameState 
+{
+    GAME_ACTIVE,
+    GAME_MENU,
+    GAME_WIN
+};
+
+// Game-related state and functionality.
+// Combines game-related data into a single class
+// for easier access to components and manageability.
+class Game 
+{
+    public:
+        // state
+        GameState       State;
+        bool            Keys[1024];
+        unsigned int    Width, Height;
+
+        // constructor/destructor
+        Game(unsigned int width, unsigned int height);
+        ~Game();
+
+        // initialize game state (load shaders/textures/levels)
+        void Init();
+
+        // game loop methods
+        void ProcessInput(float dt);
+        void Update(float dt);
+        void Render();
+};
 
 #endif
