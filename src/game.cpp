a5eba0b2b5046bcb40a3275fb57bd9bcfb13d6cf
carlwiede <carlwie@student.chalmers.se>
2022-03-25 01:20:11 +0100

    got rendering of awesomeface working

--- src/game.cpp
+++ src/game.cpp
@@ -1,4 +1,9 @@
 #include "game.h"
+#include "resource_manager.h"
+#include "sprite_renderer.h"
+
+// Game-related state data
+SpriteRenderer *Renderer;
 
 Game::Game(unsigned int width, unsigned int height) 
     : State(GAME_ACTIVE), Keys(), Width(width), Height(height)
@@ -8,12 +13,24 @@ Game::Game(unsigned int width, unsigned int height)
 
 Game::~Game()
 {
-
+    delete Renderer;
 }
 
 void Game::Init()
 {
-
+    // load shaders
+    ResourceManager::LoadShader("B:\\Documents\\Side\\cheekibreaki\\src\\sprite.vs", "B:\\Documents\\Side\\cheekibreaki\\src\\sprite.fs", nullptr, "sprite");
+    // configure shaders
+    glm::mat4 projection = glm::ortho(0.0f, static_cast<float>(this->Width), 
+        static_cast<float>(this->Height), 0.0f, -1.0f, 1.0f);
+    ResourceManager::GetShader("sprite").Use().SetInteger("image", 0);
+    ResourceManager::GetShader("sprite").SetMatrix4("projection", projection);
+    // set render-specific controls
+    Shader myShader;
+    myShader = ResourceManager::GetShader("sprite");
+    Renderer = new SpriteRenderer(myShader);
+    // load textures
+    ResourceManager::LoadTexture("B:\\Documents\\Side\\cheekibreaki\\textures\\awesomeface.png", true, "face");
 }
 
 void Game::Update(float dt)
@@ -28,5 +45,7 @@ void Game::ProcessInput(float dt)
 
 void Game::Render()
 {
-    
+    Texture2D myTexture;
+    myTexture = ResourceManager::GetTexture("face");
+    Renderer->DrawSprite(myTexture, glm::vec2(200.0f, 200.0f), glm::vec2(300.0f, 400.0f), 45.0f, glm::vec3(0.0f, 1.0f, 0.0f));
 }
