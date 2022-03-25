b50fb324da4fbb75edf26777cf14c6c365733d3b
carlwiede <carlwie@student.chalmers.se>
2022-03-25 20:07:07 +0100

    added gameobject files to serve as an abstraction of object in the game

---
+++ src/game_object.h
@@ -0,0 +1,33 @@
+#ifndef GAMEOBJECT_H
+#define GAMEOBJECT_H
+
+#include "../include/glad/glad.h"
+#include "../include/glm/glm.hpp"
+
+#include "texture.h"
+#include "sprite_renderer.h"
+
+// A container object for storing relevant information
+// about a single object entity in the game.
+class GameObject
+{
+    public:
+        // object state
+        glm::vec2   Position, Size, Velocity;
+        glm::vec3   Color;
+        float       Rotation;
+        bool        IsSolid;
+        bool        Destroyed;
+
+        // render state
+        Texture2D   Sprite;
+
+        // constructor(s)
+        GameObject();
+        GameObject(glm::vec2 pos, glm::vec2 size, Texture2D sprite, glm::vec3 color = glm::vec3(1.0f), glm::vec2 velocity = glm::vec2(0.0f, 0.0f));
+
+        // draw sprite
+        virtual void Draw(SpriteRenderer &renderer);
+};
+
+#endif
